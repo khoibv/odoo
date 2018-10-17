b130830e1b89d3d7438240834a3ef5ab8d2bf212
Khoi Bui Van <buivankhoi@gmail.com>
2018-10-17 08:40:50 +0700

    Add more props

--- nev/models/models.py
+++ nev/models/models.py
@@ -9,8 +9,8 @@ class Course(models.Model):
 
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
-    responsible_id = fields.Many2one('res.users', ondelete='set null', string="Responsible", index=True)
-    session_ids = fields.One2many('nev.session', 'course_id', string="Sessions")
+    responsible_id = fields.Many2one(comodel_name='res.users', ondelete='set null', string="Responsible", index=True)
+    session_ids = fields.One2many(comodel_name='nev.session', inverse_name='course_id', string="Sessions")
 
     @api.multi
     def copy(self, default=None):
