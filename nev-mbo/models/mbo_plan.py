a17830585e7b4da9bccc9d3ff84b0e9154af512c
khoibv <buivankhoi@gmail.com>
2018-10-18 07:23:47 +0700

    Add demo data

--- nev-mbo/models/mbo_plan.py
+++ nev-mbo/models/mbo_plan.py
@@ -2,16 +2,24 @@
 
 from odoo import models, fields, api, _
 
+PLAN_STATUSES = [
+    ('none', 'None'),
+    ('updating', 'Updating'),
+    ('wait for review', 'Wait For Review'),
+    ('approved', 'Approved'),
+    ('doing', 'Doing'),
+    ('evaluating', 'Evaluating'),
+    ('finished', 'Finished')
+]
+
 
 class MboPlan(models.Model):
     _name = 'nev.mbo.mbo_plan'
 
     name = fields.Char(required=True)
     no = fields.Integer()
-    status = fields.Selection(
-        [('none', 'None'), ('updating', 'Updating'), ('wait for review', 'Wait For Review'), ('approved', 'Approved'),
-         ('doing', 'Doing'), ('evaluating', 'Evaluating'), ('finished', 'Finished')],
-        string=_("Status Of MBO"),
-        required=True, default='updating')
+    status = fields.Selection(selection=PLAN_STATUSES,
+                              string=_("Status"),
+                              required=True, default='updating')
 
     mbo_ids = fields.One2many(comodel_name='nev.mbo.mbo', inverse_name='mbo_plan_id', string="MBO list")
